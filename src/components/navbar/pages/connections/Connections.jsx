c0002697c82f408a7f4e6e2ff2f83f23143543e8
SnehaMane17 <snehamane171998@gmail.com>
2024-02-25 19:44:24 +0530

    connection page added

---
+++ src/components/navbar/pages/connections/Connections.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Card } from 'react-bootstrap';
+import './Connections.css'
+
+ const Connections = () => {
+  return (
+    <>
+     <div className="connections-card-container d-flex flex-column justify-content-center align-items-center flex " >
+      <Card className={`connections`}>
+        <Card.Body>
+     
+        </Card.Body>
+      </Card>   
+      </div>
+    </>
+  )
+}
+
+export default Connections
