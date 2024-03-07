98f904e860999c4714fb61d6b14336ce4e955975
SnehaMane17 <snehamane171998@gmail.com>
2024-03-07 21:59:58 +0530

    post added to firebase

--- src/components/navbar/pages/home/posts/PostStatus.jsx
+++ src/components/navbar/pages/home/posts/PostStatus.jsx
@@ -1,20 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect , useMemo} from "react";
 import "./PostStatus.css";
-import { Card } from "react-bootstrap";
+import { Card, CardBody } from "react-bootstrap";
+import { toast, ToastContainer  } from 'react-toastify';
 import ModalPost from "./modal/ModalPost";
+import { useUserAuth } from "../../../../../context/UserAuthContext";
 
-export default function HomePage() {
-  const [modalOpen, setModalOpen] = useState(false)
+export default function PostStatus() {
+  const { postStatus, getStatus } = useUserAuth();
+  const [modalOpen, setModalOpen] = useState(false);
+  const [status, setStatus] = useState("");
+  const [allStatus, setAllStatus] = useState([])
+  const sendStatus = async () => {
+    await postStatus(status);
+    setModalOpen(false);
+    setStatus("");
+    toast.success('Post added successfully')
+  };
+useMemo (()=>{
+  getStatus(setAllStatus)
+})
+
   return (
     <>
       <Card className="post-status-main">
         <Card.Body>
           <div className="post-status">
-            <button className="open-post-modal" onClick={()=> setModalOpen(true)}>Start Post</button>
+            <button
+              className="open-post-modal"
+              onClick={() => setModalOpen(true)}
+            >
+              Start Post
+            </button>
           </div>
         </Card.Body>
-        <ModalPost modalOpen={modalOpen} setModalOpen={setModalOpen}/>
+        <ModalPost
+          sendStatus={sendStatus}
+          setStatus={setStatus}
+          status={status}
+          modalOpen={modalOpen}
+          setModalOpen={setModalOpen}
+        />
+      
       </Card>
+      <Card className="post-list">
+        <CardBody>
+        {allStatus.map((posts)=>{
+          return (
+            <>
+            <p>{posts.status}</p>
+            </>
+          )
+        })}
+        </CardBody>
+      </Card>
+      <ToastContainer/>
     </>
   );
 }
--- src/components/navbar/pages/home/posts/modal/ModalPost.jsx
+++ src/components/navbar/pages/home/posts/modal/ModalPost.jsx
@@ -1,9 +1,15 @@
 import Modal from "react-bootstrap/Modal";
 import "./Modal.css";
 import Button from "react-bootstrap/Button";
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes } from "react-icons/fa";
 
-export default function ModalPost({ modalOpen, setModalOpen }) {
+export default function ModalPost({
+  modalOpen,
+  setModalOpen,
+  setStatus,
+  status,
+  sendStatus,
+}) {
   return (
     <Modal
       show={modalOpen}
@@ -17,15 +23,28 @@ export default function ModalPost({ modalOpen, setModalOpen }) {
         <Modal.Title id="contained-modal-title-vcenter">
           Create a post
         </Modal.Title>
-        <FaTimes onClick={() => setModalOpen(false)} className="custom-button" />
+        <FaTimes
+          onClick={() => setModalOpen(false)}
+          className="custom-button"
+        />
       </Modal.Header>
       <Modal.Body>
-        <input 
-         className="modal-input"
-         placeholder="What do you want to talk about?" />
+        <input
+          className="modal-input"
+          placeholder="What do you want to talk about?"
+          onChange={(e) => setStatus(e.target.value)}
+          value={status}
+        />
       </Modal.Body>
       <Modal.Footer className="custom-modal-footer">
-        <Button className="post-btn" > Post </Button>
+        <Button
+          className="post-btn"
+          disabled={status.length > 0 ? false : true}
+          onClick={sendStatus}
+        >
+          {" "}
+          Post{" "}
+        </Button>
       </Modal.Footer>
     </Modal>
   );
