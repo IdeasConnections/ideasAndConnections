3b4684b13393f64fec62716e1d16e0ec4eb59f5d
SnehaMane17 <snehamane171998@gmail.com>
2024-03-10 13:04:55 +0530

    small_change

--- src/components/navbar/pages/home/HomePage.jsx
+++ src/components/navbar/pages/home/HomePage.jsx
@@ -1,21 +1,16 @@
 import React, { useState } from "react";
-import './Home.css'
-import { Card } from 'react-bootstrap';
-import PostStatus from './posts/PostStatus'
+import "./Home.css";
+import { Card } from "react-bootstrap";
+import PostStatus from "./posts/PostStatus";
 
 export default function HomePage() {
-
-
   return (
     <>
       <Card className={`home `}>
         <Card.Body>
-         <PostStatus/>
+          <PostStatus />
         </Card.Body>
-      </Card>   
-     
-   
-
+      </Card>
     </>
   );
 }
