a19ed57a036d0fa9e6596cb0c489ab20b7dd985e
SnehaMane17 <snehamane171998@gmail.com>
2024-03-25 20:25:50 +0530

    pages created for post and news

---
+++ src/components/navbar/pages/home/news/NewsCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+export default function NewsCard() {
+  return (
+    <>
+      <Card className='news-card'>
+        <Card.Body>
+          News Card
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
---
+++ src/components/navbar/pages/home/profilecard/ProfileCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+export default function ProfileCard() {
+  return (
+    <>
+      <Card className='profile-card'>
+        <Card.Body>
+          Profile Card
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
