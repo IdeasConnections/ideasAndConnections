f308efd1eaeadb81d483a1aa33a7c1ac404afc3b
SnehaMane17 <snehamane171998@gmail.com>
2024-04-09 23:06:35 +0530

    changes

---
+++ src/components/navbar/pages/home/news-section/News.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+import { Card } from "react-bootstrap";
+
+
+export default function News() {
+  return (
+    <>
+      <Card className="news">
+      <Card.Body>
+    News Section
+      </Card.Body>
+    </Card>
+    </>
+  );
+}
