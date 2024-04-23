2670a72b8967a3cac789843bed3967df1238698c
SnehaMane17 <snehamane171998@gmail.com>
2024-04-23 23:31:53 +0530

    config changes

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+    apiKey: "0edd1cdec288d3587627f6838f4f3dea"
+  };
+  
+  export default config;
+  
--- src/components/navbar/pages/home/news-section/News.jsx
+++ src/components/navbar/pages/home/news-section/News.jsx
@@ -1,25 +1,18 @@
 import React, { useState, useEffect } from "react";
 import "./News.css";
 import { Card } from "react-bootstrap";
+import config from "../../../../../../config";
 
 export default function News() {
-  const  apikey = "0edd1cdec288d3587627f6838f4f3dea";
-  const category = "general";
- const  url =
-    "https://gnews.io/api/v4/top-headlines?category=" +
-    category +
-    "&lang=en&country=us&max=10&apikey=" +
-    apikey;
-
   const [articles, setArticles] = useState([]);
   const fetchArticles = async () => {
-    const apikey = "0edd1cdec288d3587627f6838f4f3dea";
+    const { apiKey } = config;
     const category = "general";
     const url =
       "https://gnews.io/api/v4/top-headlines?category=" +
       category +
       "&lang=en&country=us&max=10&apikey=" +
-      apikey;
+      apiKey;
 
     try {
       const response = await fetch(url);
@@ -31,23 +24,27 @@ export default function News() {
   };
 
   useEffect(() => {
-    fetchArticles(); 
+    fetchArticles();
     const intervalId = setInterval(() => {
       fetchArticles();
-    }, 3600000 );
+    }, 3600000);
 
     return () => clearInterval(intervalId);
-  }, []); 
+  }, []);
   return (
     <>
       <Card className="news">
         <Card.Body>
           <p className="heading">Top News</p>
-          <ul  className="article-list">
+          <ul className="article-list">
             {articles.map((article, index) => (
               <li key={index}>
                 <h3>
-                  <a href={article.url} target="_blank" rel="noopener noreferrer">
+                  <a
+                    href={article.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     {article.title}
                   </a>
                 </h3>
