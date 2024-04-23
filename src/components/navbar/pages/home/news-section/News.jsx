b7e2c9776b34a9d0dd56505268cda593f208a40f
SnehaMane17 <snehamane171998@gmail.com>
2024-04-23 22:55:15 +0530

    news api integrated

--- src/components/navbar/pages/home/news-section/News.jsx
+++ src/components/navbar/pages/home/news-section/News.jsx
@@ -1,16 +1,53 @@
-import React, { useState } from "react";
-import './News.css'
+import React, { useState, useEffect } from "react";
+import "./News.css";
 import { Card } from "react-bootstrap";
 
-
 export default function News() {
+  const  apikey = "0edd1cdec288d3587627f6838f4f3dea";
+  const category = "general";
+ const  url =
+    "https://gnews.io/api/v4/top-headlines?category=" +
+    category +
+    "&lang=en&country=us&max=10&apikey=" +
+    apikey;
+
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+         url
+        );
+        const data = await response.json();
+        console.log(data, 'data')
+        setArticles(data.articles);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <Card className="news">
-      <Card.Body>
-    News Section
-      </Card.Body>
-    </Card>
+        <Card.Body>
+          <p className="heading">Top News</p>
+          <ul  className="article-list">
+            {articles.map((article, index) => (
+              <li key={index}>
+                <h3>
+                  <a href={article.url} target="_blank" rel="noopener noreferrer">
+                    {article.title}
+                  </a>
+                </h3>
+                {/* <p>{article.description}</p> */}
+              </li>
+            ))}
+          </ul>
+        </Card.Body>
+      </Card>
     </>
   );
 }
