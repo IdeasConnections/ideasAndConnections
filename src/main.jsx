ee9bb6d5eb7e269fc930e9e2cad6bef67ea75d43
SnehaMane17 <snehamane171998@gmail.com>
2024-05-01 13:10:48 +0530

    mobile view removed

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,32 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { isMobile, isTablet } from 'react-device-detect';
+import { TbAlertTriangle } from "react-icons/tb";
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    {" "}
-    <App />
-  </BrowserRouter>
-);
+const renderApp = () => {
+  if (isMobile || isTablet) {
+
+    ReactDOM.createRoot(document.getElementById("root")).render(
+      <div className="container">
+        <div className="content">  
+          <TbAlertTriangle size={30} style={{color: 'yellow'}} />
+         <h1>Please use a desktop to view this site.</h1>
+       {/* <h1> Mobile view is not available.Thanks!</h1> */}
+
+        </div>
+      
+      </div>
+      
+    );
+  } else {
+    ReactDOM.createRoot(document.getElementById("root")).render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+  }
+};
+
+renderApp();
